78f32ac673feec5ed4eee9c62274c9a71157ee67
themdshakibul <mdshakibul88240@gmail.com>
2026-02-10 19:18:27 +0600

    function

---
+++ H. Functions (48–55)/function.js
@@ -0,0 +1,40 @@
+// 48. দুটি সংখ্যার যোগফল বের করার function লেখো
+//! Ans:
+function twoSumOfNumbers(num1, num2) {
+  total = num1 + num2;
+  return total;
+}
+
+console.log(twoSumOfNumbers(20, 40));
+
+// 49. even/odd চেক করার function লেখো
+//! Ans:
+function numberOfEvenOdd(number) {
+  for (let i = 1; i < number; i++) {
+    if (number % 2 === 0) {
+      return "Even Numbers";
+    } else {
+      return "Odd Numbers";
+    }
+  }
+}
+
+console.log(numberOfEvenOdd(20));
+
+// 50. array এর সব সংখ্যার sum বের করার function লেখো
+//! Ans:
+
+// 51. string reverse করার function লেখো
+//! Ans:
+
+// 52. leap year check function লেখো
+//! Ans:
+
+// 53. array থেকে largest number বের করার function লেখো
+//! Ans:
+
+// 54. unit convert করার function লেখো (inch → feet)
+//! Ans:
+
+// 55. calculator function বানাও (add, sub, mul, div)
+//! Ans:
