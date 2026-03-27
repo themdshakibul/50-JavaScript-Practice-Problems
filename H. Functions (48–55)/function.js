3791a60de6b704e286d0d3dc197d9d1845221c7f
themdshakibul <mdshakibul88240@gmail.com>
2026-03-27 20:55:08 +0600

    complete

--- H. Functions (48–55)/function.js
+++ H. Functions (48–55)/function.js
@@ -23,18 +23,53 @@ console.log(numberOfEvenOdd(20));
 
 // 50. array এর সব সংখ্যার sum বের করার function লেখো
 //! Ans:
+function gotSum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+let gotSumArray = gotSum([120, 30, 50, 60]);
+console.log(gotSumArray);
 
 // 51. string reverse করার function লেখো
 //! Ans:
+function stringReverse(arr) {
+  rev = "";
+  for (let cha of arr) {
+    rev = cha + rev;
+  }
+  return rev;
+}
+
+let nameReverse = stringReverse("Md Rokibul Islam");
+console.log(nameReverse);
 
 // 52. leap year check function লেখো
 //! Ans:
+function leapYear(year) {
+  if (year % 2 === 0 && 4 === 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+console.log(leapYear(2003));
 
 // 53. array থেকে largest number বের করার function লেখো
 //! Ans:
+function largestNumberd(arr) {
+  return Math.max(...arr);
+}
+const numbers = [100, 200, 400, 500];
+console.log(largestNumberd(numbers));
 
 // 54. unit convert করার function লেখো (inch → feet)
 //! Ans:
 
 // 55. calculator function বানাও (add, sub, mul, div)
 //! Ans:
+
+
